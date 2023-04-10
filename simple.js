ec2dee8dc222ad65b1bbaaa4b461ab8a1ca224da
jesminjes <mstjesmina56@gmail.com>
2023-04-10 22:23:52 +0600

    simple.js

---
+++ simple.js
@@ -0,0 +1,8 @@
+var firstLine = ['Iam going to be'];
+var secLine = ['an awesome web developer'];
+var twoLines = firstLine + ' ' + secLine;
+console.log(twoLines);
+
+var number = 119;
+var reminder = number % 5;
+console.log(reminder);
