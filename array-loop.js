78bac9a6ef5961f837dff775ced8f590be75885d
jesminjes <mstjesmina56@gmail.com>
2023-04-10 16:57:44 +0600

    araay-loop

---
+++ array-loop.js
@@ -0,0 +1,12 @@
+var numbers = [34, 45, 32, 87, 90, 86, 83, 12, 33, 67];
+
+for(var i = 0; i < numbers.length; i++ ){
+    var number = numbers[i];
+    console.log(number);
+}
+
+var items = ['bottle', 'mouse', 'sunglass', 'pen', 'notebook'];
+for(var i = 0; i < items.length; i++){
+    var item = items[i];
+    console.log(item);
+}
