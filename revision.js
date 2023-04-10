d3acc289ceeebb8b9804b1161da3133bcdd7da12
jesminjes <mstjesmina56@gmail.com>
2023-04-10 12:54:49 +0600

    while loop

---
+++ revision.js
@@ -0,0 +1,28 @@
+// string type varriabe
+var bottole = 'water';
+var bottoleColor = 'yellow';
+// number type variable 
+var sunglassPrice = 120;
+var penColor = 'blue';
+// boolean type varriable
+var isExpensive = true;
+
+var tableItems = ['bottle', 'pen', 'mouse', 'sunglass', 'paper' ]
+var item3 = tableItems[3];
+tableItems[4] = 'notebook';
+var penIndex = tableItems.indexOf('pen')
+
+// push pop
+
+
+// conditionals
+
+if(tableItems.lengthn > 4){
+
+}
+else if (tableItems[3] == 'pen'  ){
+
+}
+else{
+    
+}
